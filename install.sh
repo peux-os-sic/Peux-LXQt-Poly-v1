0e141a002c731ef5222af018a4165161ab07d8b9
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-04-29 17:33:49 +0530

    commit

--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-dir="$HOME/.config/polybar"
+###########################################################
+# Author - DN-debug
+# Description - Peux OS LXQt Polybar configuration script
+###########################################################
 
-# check if polybar directory exists or not!
-if [[ ! -d $dir ]]; then
-    # using two step process instead of one liner
-    mkdir $dir
-    mkdir -p $dir/LXQt/v1
+if zenity --question --text="Want to proceed?"
+then 
+
+    dir="$HOME/.config/polybar"
+
+    # check if polybar directory exists or not!
+    if [[ ! -d $dir ]]; then
+        # using two step process instead of one liner
+        mkdir $dir
+        mkdir -p $dir/LXQt/v1
+    else
+        mkdir -p $dir/LXQt/v1 # one liner when parent folder is not available
+    fi
+
+
+    echo "changing directory"
+    cd ../
+
+    cp -r Peux-LXQt-Poly-v1/polybar/ $dir/LXQt/v1
+    notify-send 'Fetched folder has been moved to ~/.config/polybar/LXQt/'
+
 else
-    mkdir -p $dir/LXQt/v1 # one liner when parent folder is not available
+    notify-send "Operation skipped!"
 fi
 
-
-echo "changing directory"
-cd ../
-
-cp -r Peux-LXQt-Poly-v1/polybar/ $dir/LXQt/v1
-rm -rf Peux-LXQt-Poly-v1
-notify-send 'Fetched folder has been moved to ~/.config/polybar/LXQt/'
-
+rm -rf /tmp/fetcher/Peux-LXQt-Poly-v1
+notify-send "Done!"
